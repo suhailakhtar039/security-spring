4b3664a1a5f78d7384b55a9b2e9eb7ad932bb06d
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-27 22:19:23 +0530

    imported necessary module

--- BankApplication/src/app/navbar/header/header.ts
+++ BankApplication/src/app/navbar/header/header.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
-
-}
+export class Header {}
