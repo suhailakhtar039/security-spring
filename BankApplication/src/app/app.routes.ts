dabb06776f271dea67dc03bb0edf15874186a690
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-28 14:40:47 +0530

    route for contact us created

--- BankApplication/src/app/app.routes.ts
+++ BankApplication/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Body } from './body/body';
 import { Login } from './login/login';
+import { ContactUs } from './contact-us/contact-us';
 
 export const routes: Routes = [
   { path: '', component: Body, pathMatch: 'full' },
   { path: 'login', component: Login, pathMatch: 'full' },
+  { path: 'contact', component: ContactUs, pathMatch: 'full' },
 ];
