a435ba3074f9e742a36114c85b5827ffc68c0675
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-28 14:41:34 +0530

    contact us form using form builder created

--- BankApplication/src/app/contact-us/contact-us.ts
+++ BankApplication/src/app/contact-us/contact-us.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+  ],
   templateUrl: './contact-us.html',
   styleUrl: './contact-us.css',
 })
 export class ContactUs {
+  contactUsForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactUsForm = this.fb.group({
+      name: [''],
+      email: ['', [Validators.email]],
+      subject: [''],
+      message: [''],
+    });
+  }
+
+  onSubmitofContactUs() {
+    console.log(this.contactUsForm.value);
+  }
 }
