cb966cd9f2b1dac00ac980190d2f15fb819ae4b3
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-28 12:45:26 +0530

    adding route

--- BankApplication/src/app/app.routes.ts
+++ BankApplication/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Body } from './body/body';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Body, pathMatch: 'full' },
+  { path: 'login', component: Login, pathMatch: 'full' },
+];
