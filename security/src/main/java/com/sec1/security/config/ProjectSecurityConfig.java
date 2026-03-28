ce4c325bc98b31a7dec693327b25f4b024e983cc
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-28 18:56:48 +0530

    created user in memory without any password encoder

--- security/src/main/java/com/sec1/security/config/ProjectSecurityConfig.java
+++ security/src/main/java/com/sec1/security/config/ProjectSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,5 +18,16 @@ public class ProjectSecurityConfig {
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    /**
+     * Always remember to choose {noop} if you want to provide password as it is. For example in my
+     * scenario, I want to pass password as 12345
+     */
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
