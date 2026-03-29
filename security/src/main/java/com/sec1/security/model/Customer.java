237f286358ea3f72d6e9eb7d013924d4d634eba7
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-29 15:41:19 +0530

    Customer model is created

---
+++ security/src/main/java/com/sec1/security/model/Customer.java
@@ -0,0 +1,20 @@
+package com.sec1.security.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "customer")
+@Data
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String pwd;
+    private String role;
+
+}
