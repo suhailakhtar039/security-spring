80e0650d7abe994c13b7e5928097e8e885168622
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-29 15:13:54 +0530

    removed custom user and admin from ProjectSecurityConfig

--- security/src/main/java/com/sec1/security/config/ProjectSecurityConfig.java
+++ security/src/main/java/com/sec1/security/config/ProjectSecurityConfig.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -29,19 +29,8 @@ public class ProjectSecurityConfig {
      * scenario, I want to pass password as 12345
      */
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user = User
-                .withUsername("user")
-                .password("{noop}User@786$^yr")
-                .authorities("read")
-                .build();
-
-        UserDetails admin = User
-                .withUsername("admin")
-                .password("{bcrypt}$2a$12$zeg.jTTBKjRZEvXFlR1MPOuBqJD37BGmSNXTF2WhoQiBFdDic45Cm")
-                .authorities("admin")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
