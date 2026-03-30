5401da5b4fac7342cbf88ad0a981d4b2a9f02b8e
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-30 20:05:43 +0530

    UserDetailsService bean commented as not to cause conflict

--- security/src/main/java/com/sec1/security/config/ProjectSecurityConfig.java
+++ security/src/main/java/com/sec1/security/config/ProjectSecurityConfig.java
@@ -4,14 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-
-import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -32,10 +28,10 @@ public class ProjectSecurityConfig {
      * Always remember to choose {noop} if you want to provide password as it is. For example in my
      * scenario, I want to pass password as 12345
      */
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
