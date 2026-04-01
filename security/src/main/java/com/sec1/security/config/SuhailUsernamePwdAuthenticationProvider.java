3da6bd1dfeeebee128c57dd5900748d554a81f69
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-04-01 20:17:24 +0530

    enabling both the function that we got via AuthenticationProvider

--- security/src/main/java/com/sec1/security/config/SuhailUsernamePwdAuthenticationProvider.java
+++ security/src/main/java/com/sec1/security/config/SuhailUsernamePwdAuthenticationProvider.java
@@ -1,18 +1,38 @@
 package com.sec1.security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class SuhailUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public @Nullable Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if (passwordEncoder.matches(pwd, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(username, pwd, userDetails.getAuthorities());
+        } else {
+            throw new BadCredentialsException("Invalid Password!");
+        }
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return false;
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
     }
 }
