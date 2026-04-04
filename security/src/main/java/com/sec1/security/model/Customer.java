0504211aa64ed17ec3df4da398001b5337b593ae
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-04-04 13:20:11 +0530

    created this models corresponding to tables

--- security/src/main/java/com/sec1/security/model/Customer.java
+++ security/src/main/java/com/sec1/security/model/Customer.java
@@ -1,20 +1,41 @@
 package com.sec1.security.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
 
 @Entity
-@Table(name = "customer")
-@Data
+@Getter
+@Setter
 public class Customer {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
     private long id;
-    private String pwd;
-    private String role;
+
+    private String name;
+
     private String email;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String pwd;
+
+    private String role;
+
+    @Column(name = "create_dt")
+    @JsonIgnore
+    private Date createDt;
+
 }
