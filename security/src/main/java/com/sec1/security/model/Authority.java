a33ed6bccc7f33b20bb541ca0c14eefc84fe5f46
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-04-05 22:30:27 +0530

    authority model created with relation between customer as well

---
+++ security/src/main/java/com/sec1/security/model/Authority.java
@@ -0,0 +1,25 @@
+package com.sec1.security.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="authorities")
+@Data
+public class Authority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name="customer_id")
+    private Customer customer;
+}
